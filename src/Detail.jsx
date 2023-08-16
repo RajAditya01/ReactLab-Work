3c5021af227a66f339407ffd7a90e554414df869
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-16 16:57:13 +0530

    01

---
+++ src/Detail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Detail() {
+    const name="Aditya";
+const age=20;
+    return (
+        <>
+            <h1>My Name is {name} </h1>
+            <h2>I Am {age} Year Old</h2>
+        </>
+    );
+}
+
+export default Detail;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Detail from "./Detail";
+ReactDOM.render(
+    <>
+    <Detail/>
+    </>,
+    document.getElementById("root")
+);
