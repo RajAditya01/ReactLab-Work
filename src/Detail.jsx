52c4fe3da3475556b92dd039c14260852bef948b
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-16 17:09:10 +0530

    01

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -14,7 +14,9 @@
 // export default Detail;
 
 
+// 2
 import React, { useState } from "react";
+import "./Detail.css"; // Import the CSS file
 
 function Detail() {
     const [name, setName] = useState(""); // Initialize name state with an empty string
@@ -22,20 +24,26 @@ function Detail() {
 
     return (
         <>
-            <input
-                type="text"
-                placeholder="Enter your name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <input
-                type="text"
-                placeholder="Enter your age"
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
-            />
-            <h1>My Name is {name}</h1>
-            <h2>I Am {age} Year Old</h2>
+            <div className="input-container">
+                <input
+                    type="text"
+                    placeholder="Enter your name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div className="input-container">
+                <input
+                    type="text"
+                    placeholder="Enter your age"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                />
+            </div>
+            <div className="result">
+                <h1>My Name is {name}</h1>
+                <h2>I Am {age} Year Old</h2>
+            </div>
         </>
     );
 }
