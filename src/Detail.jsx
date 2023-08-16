ed92b2f883c7b934dd77571c6c8ce904735cf322
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-16 17:02:56 +0530

    01

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+// import React from "react";
+
+// function Detail() {
+//     const name="Aditya";
+// const age=20;
+//     return (
+//         <>
+//             <h1>My Name is {name} </h1>
+//             <h2>I Am {age} Year Old</h2>
+//         </>
+//     );
+// }
+
+// export default Detail;
+
+
+import React, { useState } from "react";
 
 function Detail() {
-    const name="Aditya";
-const age=20;
+    const [name, setName] = useState(""); // Initialize name state with an empty string
+    const [age, setAge] = useState("");   // Initialize age state with an empty string
+
     return (
         <>
-            <h1>My Name is {name} </h1>
+            <input
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Enter your age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+            />
+            <h1>My Name is {name}</h1>
             <h2>I Am {age} Year Old</h2>
         </>
     );
