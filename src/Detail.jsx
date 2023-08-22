795b655d0674268bb12dc8b31f0b09d91de90734
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-22 22:28:45 +0530

    .

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,51 +1,48 @@
 // import React from "react";
 
-// function Detail() {
-//     const name="Aditya";
-// const age=20;
+// function StudentDetails() {
+//     const name = "Aditya Raj";
+//     const rollNo = "11212714";
+//     const branch = "Computer Science";
+//     const age = 20;
+
 //     return (
 //         <>
-//             <h1>My Name is {name} </h1>
-//             <h2>I Am {age} Year Old</h2>
+//             <h1>Student Details</h1>
+//             <p>Name: {name}</p>
+//             <p>Roll Number: {rollNo}</p>
+//             <p>Branch: {branch}</p>
+//             <p>Age: {age}</p>
 //         </>
 //     );
 // }
 
-// export default Detail;
+// export default StudentDetails;
 
 
-// 2
-import React, { useState } from "react";
+import React from "react";
 import "./Detail.css"; // Import the CSS file
 
-function Detail() {
-    const [name, setName] = useState(""); // Initialize name state with an empty string
-    const [age, setAge] = useState("");   // Initialize age state with an empty string
+function StudentDetails() {
+  const name = "Aditya Raj";
+  const rollNo = "11212714";
+  const branch = "Computer Science";
+  const age = 20;
 
-    return (
-        <>
-            <div className="input-container">
-                <input
-                    type="text"
-                    placeholder="Enter your name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-            </div>
-            <div className="input-container">
-                <input
-                    type="text"
-                    placeholder="Enter your age"
-                    value={age}
-                    onChange={(e) => setAge(e.target.value)}
-                />
-            </div>
-            <div className="result">
-                <h1>My Name is {name}</h1>
-                <h2>I Am {age} Year Old</h2>
-            </div>
-        </>
-    );
+  return (
+    <div className="student-details-container"> {/* Apply CSS class */}
+      <h1 className="student-name">Student Details</h1> {/* Apply CSS class */}
+      <p className="detail-label">Name:</p>
+      <p className="detail-value">{name}</p>
+      <p className="detail-label">Roll Number:</p>
+      <p className="detail-value">{rollNo}</p>
+      <p className="detail-label">Branch:</p>
+      <p className="detail-value">{branch}</p>
+      <p className="detail-label">Age:</p>
+      <p className="detail-value">{age}</p>
+    </div>
+  );
 }
 
-export default Detail;
+export default StudentDetails;
+
